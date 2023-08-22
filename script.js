8110d6c47ba145be0dd0650a3fc54010435cb0ef
Gabriel Fellipe Marquardt <119713462+DevMarquardt@users.noreply.github.com>
2023-08-22 14:42:47 +0000

    vamo ve ne

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let count = 0;
 
 for(let element of elements){
     element.addEventListener("click", event => {
-        count ++ ;
         let temPeca = verificaPecaTem(event.target.innerHTML);
         if(temPeca){
+            count ++ ;
             if(count % 2 == 0){
                 event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
             } else{
@@ -26,25 +26,11 @@ function verificaPecaTem(event){
 }
 
 function verificaVitoria(){
-    let mapa = elements
-    for(var element of mapa){
-        if(element.innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'){
-            element = 1
-        }else if(element.innerHTML == '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>'){
-            element = 2
-        }else{
-            element = 0
-        }
-    }
-
-    verificaVitoriaPT2(mapa)
-    
-}
-
-
-function verificaVitoriaPT2(mapa){
-    if(mapa[0] == 1 && mapa[1] == 1 && mapa[2] == 1){
+    if(elements[0].innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'
+     && elements[1].innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'
+      && elements[2].innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'){
         alert('vitoria')
     }
 }
+
 
