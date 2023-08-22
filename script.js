282f07252115fbe51855569cdc3ee9898582053d
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-21 21:52:39 -0300

    aa

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ let count = 0;
 for(let element of elements){
     element.addEventListener("click", event => {
         count ++ ;
-        if(verificaPecaAntiga(event.target.innerHTML)){
-            event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
-        } else{
-            event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
+        let temPeca = verificaPecaTem(event.target.innerHTML);
+        if(!temPeca){
+            if(count % 2 == 0){
+                event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
+            } else{
+                event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
+            }
         }
     })
 }
 
-function verificaPecaAntiga(event){
-    if(event == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'){
-        return true;
-    }else{
+function verificaPecaTem(event){
+    if(event != ""){
         return false;
+    } else{
+        return true;
     }
 }
 
