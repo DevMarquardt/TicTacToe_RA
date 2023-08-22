dddb1e25a072fa59bbc4984a864ba8e6d10b12c1
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-21 21:32:44 -0300

    boooooora

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let elements = document.getElementsByClassName("quadrado");
 
+let count = 0;
+
 for(let element of elements){
     element.addEventListener("click", event => {
+        count ++ ;
+        if(count % 2 == 0){
             event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.2" radius="20"></a-box>';
+        } else{
+            event.target.innerHTML += '<a-circle color="blue"  width="0.09" height="0.2" position="0 0 0.2" radius="20"></a-circle>';
+        }
     })
 }
