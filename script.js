6b81fa205b00ebcb6a5124e566d54bf26e893389
Gabriel Fellipe Marquardt <119713462+DevMarquardt@users.noreply.github.com>
2023-08-22 15:23:55 +0000

    vamo ve ne

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ for(let element of elements){
     element.addEventListener("click", event => {
         let temPeca = verificaPecaTem(event.target.innerHTML);
         if(temPeca){
+            count ++ ;
             if(count % 2 == 0){
                 event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
             } else{
                 event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
             }
-            count ++ ;
             verificaVitoria()
         }
     })
