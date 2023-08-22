c5ed5c24dd553e6fa0d8f665e9e2dedf8361755c
Gabriel Fellipe Marquardt <119713462+DevMarquardt@users.noreply.github.com>
2023-08-22 15:06:07 +0000

    vamo ve ne

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for(let element of elements){
             } else{
                 event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
             }
-            elements.item(element).innerHTML = event.target.innerHTML 
+            elements.item(event.target).innerHTML = event.target.innerHTML 
             count ++ ;
             verificaVitoria()
         }
