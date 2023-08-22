022da07ea1b4aa876f8dfec1698ce66ba08bb9a0
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-21 21:14:45 -0300

    Tentativa de fazer aparecer o x (2)

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let elements = document.getElementsByClassName("quadrado");
 
 for(let element of elements){
     element.addEventListener("click", function(){
-        element.innerText = "X";
+        
     })
 }
