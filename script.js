50e0bbf086aeac51c188c11403ef6014def62b5f
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-21 21:30:56 -0300

    boooooora

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let elements = document.getElementsByClassName("quadrado");
 
 for(let element of elements){
-    element.addEventListener("click", function(){
-        element.innerText = `<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.2" radius="20"></a-box>`;
+    element.addEventListener("click", event => {
+            event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.2" radius="20"></a-box>';
     })
 }
