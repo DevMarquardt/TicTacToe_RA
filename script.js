cee32661e2b77b93390091d1496e56a380c336ed
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-28 18:52:27 -0300

    a

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function verificaPecaTem(element){
 function verificaVitoria(){
 
     let naoEmpate = false
-
+    alert(elements[0].innerHTML)
     if(elements[0].innerHTML === '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'){
         alert('ooooooooooo')
     }
