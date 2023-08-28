163885a518f2bc4a23bdc0f228060f99f054c2a0
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-28 17:59:38 -0300

    a

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 let elements = document.getElementsByClassName("quadrado");
-
 let count = 0;
 
-for(let element of elements){
-    element.addEventListener("click", event => {
-        let temPeca = verificaPecaTem(event.target.innerHTML);
-        if(temPeca){
-            count ++ ;
-            if(count % 2 == 0){
-                event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
-            } else{
-                event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
-            }
-            verificaVitoria()
-        }
-    })
+for (let element of elements) {
+  element.addEventListener("click", event => {
+    let temPeca = verificaPecaTem(event.target.innerHTML);
+    if (temPeca) {
+      count++;
+      if (count % 2 === 0) {
+        event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
+      } else {
+        event.target.innerHTML += '<a-box color="blue" width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
+      }
+      verificaVitoria();
+    }
+  });
 }
 
-function verificaPecaTem(event){
-    if(event != ""){
-        return false;
-    } else{
-        return true;
-    }
+function verificaPecaTem(content) {
+  return content === "";
 }
 
-function verificaVitoria(){
-    if(elements[0].innerHTML === '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'){
-        alert('ooooooooooo')
-    }
-    if(elements[0].innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'
-     && elements[1].innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'
-      && elements[2].innerHTML == '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'){
-        alert('vitoria')
-    }
+function verificaVitoria() {
+  if (
+    elements[0].innerHTML === '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>' &&
+    elements[1].innerHTML === '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>' &&
+    elements[2].innerHTML === '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>'
+  ) {
+    alert('Player 1 wins');
+  } else if (
+    elements[0].innerHTML === '<a-box color="blue" width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>' &&
+    elements[1].innerHTML === '<a-box color="blue" width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>' &&
+    elements[2].innerHTML === '<a-box color="blue" width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>'
+  ) {
+    alert('Player 2 wins');
+  }
 }
-
-
