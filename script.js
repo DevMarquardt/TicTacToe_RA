fd4e171e967928447b0a946b5ce7840cd12c56db
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2023-08-28 18:33:21 -0300

    a

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ let elements = document.getElementsByClassName("quadrado");
 let count = 0;
 
 for(let element of elements){
-    element.addEventListener("click",index,  event => {
+    element.addEventListener("click",  event => {
         let temPeca = verificaPecaTem(event.target.innerHTML);
         if(temPeca){
-            count ++ ;
             if(count % 2 == 0){
                 event.target.innerHTML += '<a-circle color="green" radius="0.1" position="0 0 0.6"></a-circle>';
             } else{
                 event.target.innerHTML += '<a-box color="blue"  width="0.09" height="0.2" position="0 0 0.08" radius="20"></a-box>';
             }
-            elements[index].innerHTML += event.target.innerHTML;
+            elements[2].innerHTML += event.target.innerHTML;
+            alert(elements[2].innerHTML)
             verificaVitoria()
         }
-        index++;
+        count ++ ;
     })
 }
 
